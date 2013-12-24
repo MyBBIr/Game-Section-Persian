e3968bcdfbf4f2c3749f860b21356a73aae8c6b1
Mehdi Karimi <bomber.man87@yahoo.com>
2013-12-24 18:40:59 +0330

    Update tournaments.lang.php
    ترجمه شد.

--- inc/languages/persian/tournaments.lang.php
+++ inc/languages/persian/tournaments.lang.php
@@ -6,6 +6,9 @@
  *   
  *   Website: http://www.gamesection.org
  *   
+ *   Persian translate by : My-bb.ir - mehdi karimi (Mellat)
+ *   Website: http://Mellat-design.ir
+ *   
  *   Last modified: 16/02/2010 by Paretje
  *
  ***************************************************************************/
@@ -27,72 +30,73 @@
  *
  ***************************************************************************/
 
-$l['tournaments'] = "Tournaments";
-$l['tournaments_stats_open'] = "<strong>{1}</strong> open tournaments";
-$l['tournaments_stats_started'] = "<strong>{1}</strong> started tournaments";
-$l['tournaments_stats_finished'] = "<strong>{1}</strong> finished tournaments";
-$l['add_tournament'] = "Create tournament";
-$l['your_tournament_stats'] = "Your tournament statistics";
-$l['tournaments_user_nostartedgames'] = "The are no started tournaments which you have joined.";
+$l['tournaments'] = "مسابقات";
+$l['tournaments_stats_open'] = "<strong>{1}</strong> باز کردن مسابقه";
+$l['tournaments_stats_started'] = "<strong>{1}</strong> مسابقه های باز شده";
+$l['tournaments_stats_finished'] = "<strong>{1}</strong> مسابقه های پایان یافته";
+$l['add_tournament'] = "ایجاد مسابقه";
+$l['your_tournament_stats'] = "آمار مسابقات شما";
+$l['tournaments_user_nostartedgames'] = "هیچ مسابقه ای که شما در آن حضور داشته اید آغاز نشده است.";
 
-$l['tournaments_open'] = "Open Tournaments";
-$l['tournaments_started'] = "Started Tournaments";
-$l['tournaments_finished'] = "Finished Tournaments";
-$l['openplaces'] = "Open places";
-$l['numberplayers'] = "Number of players";
-$l['starteddate'] = "Started on";
-$l['endeddate'] = "Ended on";
-$l['no_tournaments'] = "There are no tournaments with this status.";
+$l['tournaments_open'] = "باز کردن مسابقه";
+$l['tournaments_started'] = "مسابقه های آغاز شده";
+$l['tournaments_finished'] = "مسابقه های تمام شده";
+$l['openplaces'] = "مکان های باز";
+$l['numberplayers'] = "تعداد بازیکنان";
+$l['starteddate'] = "آغاز در";
+$l['endeddate'] = "اتمام در";
+$l['no_tournaments'] = "هیچ مسابقه ای با این وضعیت وجود ندارد.";
 
-$l['tournaments_game'] = "Game to use for the tournament:";
-$l['tournaments_rounds'] = "Rounds for the tournament:";
-$l['tournaments_roundtime'] = "Time for one round:";
-$l['tournaments_maxtries'] = "Maximum tries to play the game in one round:";
-$l['tournaments_maxtries_desc'] = "The number of tries can't be less then one, and more then 99.";
+$l['tournaments_game'] = "باز مورد استفاده برای این مسابقه :";
+$l['tournaments_rounds'] = "دفعات این مسابقه :";
+$l['tournaments_roundtime'] = "زمان هر دفعه از بازی :";
+$l['tournaments_maxtries'] = "حداکثر دعات تلاش برای بازی در این مسابقه :";
+$l['tournaments_maxtries_desc'] = "تعداد دفعات تلاش نباید کمتر از 1 و بیشتر از 99 باشد .";
 
-$l['tournaments_testgame'] = "Test game";
-$l['tournaments_rounds_sen'] = "{1} rounds";
-$l['tournaments_roundtime_sen'] = "{1} days";
+$l['tournaments_testgame'] = "تست بازی";
+$l['tournaments_rounds_sen'] = "{1} دور";
+$l['tournaments_roundtime_sen'] = "{1} روز";
 
-$l['added_tournament'] = "Tournament is added.";
+$l['added_tournament'] = "مسابقه افزوده شد .";
 
-$l['view_tournament'] = "{1} Tournament added on {2}";
-$l['tournament_infobox'] = "Tournament Information";
-$l['play_tournament'] = "Play tournament";
-$l['join_tournament'] = "Join tournament";
-$l['tournament_maxtries'] = "Maximum tries:";
-$l['tournament_roundtime'] = "Days a round:";
-$l['tournament_tries'] = "Tries: <strong>{1}</strong>/{2}";
-$l['tournament_tries_needed'] = "{1} tries needed";
-$l['tournamentdoesntexist'] = "Tournament doesn't exist.";
+$l['view_tournament'] = "{1} مسابقه اضافه شد به  {2}";
+$l['tournament_infobox'] = "اطلاعات مسابقه";
+$l['play_tournament'] = "انجام مسابقه";
+$l['join_tournament'] = "محلق شدن به مسابقه";
+$l['tournament_maxtries'] = "حداکثر دفعات تلاش:";
+$l['tournament_roundtime'] = "دور در روز :";
+$l['tournament_tries'] = "تلاش ها : <strong>{1}</strong>/{2}";
+$l['tournament_tries_needed'] = "{1} دفعه تلاش نیاز است";
+$l['tournamentdoesntexist'] = "مسابقه موجود نیست.";
 
-$l['tournamentfull'] = "The tournament is already full, you can't join anymore!";
-$l['alreadyjoined'] = "You've already joined this tournament!";
-$l['joined_tournament'] = "You've joined the tournament";
+$l['tournamentfull'] = "ظرفیت این مسابقه تکمیل شده است ، شما نمی توانید به این مسابقه ملحق شوید !";
+$l['alreadyjoined'] = "شما در حال حاضر به این مسابقه پیوسته اید !";
+$l['joined_tournament'] = "شما به مسابقه پیوسته اید.";
 
-$l['tournamentnotjoined'] = "You haven't joined this tournament.";
-$l['tournamentmaxtriesreached'] = "You've reached the maxtries of this tournament.";
-$l['tournamentended'] = "The round is ended.";
+$l['tournamentnotjoined'] = "شما در این مسابقه عضو نشدید.";
+$l['tournamentmaxtriesreached'] = "شما به حداکثر دعات تلاش برای این مسابقه رسیده اید.";
+$l['tournamentended'] = "این دور به پایان رسید .";
 
-$l['tournamentsstatistics'] = "Tournament Statistics";
-$l['tournamentswon'] = "Tournaments won:";
-$l['tournamentsjoined'] = "Tournaments joined:";
+$l['tournamentsstatistics'] = "آمار مسابقات";
+$l['tournamentswon'] = "برنده های مسابقه :";
+$l['tournamentsjoined'] = "شرکت کنندگان در مسابقه :";
 
-$l['options_tournaments'] = "Tournaments Options";
-$l['tournament_notify'] = "Notify me by email when a new tournament round in which you're playing starts.";
+$l['options_tournaments'] = "گزینه های مسابقه";
+$l['tournament_notify'] = "زمانی که مسابقه جدیدی شروع میشود مرا از طریق ایمیل باخبر کن .";
 
-$l['emailsubject_newtournamentround'] = "New Tournament Round Started at {1}";
+$l['emailsubject_newtournamentround'] = "مسابقه جدیدی در {1} آغاز میشود";
 $l['email_newtournamentround'] = "{1},
 
-Round number {3} of {4} from {5}. To play this tournament, you can follow this link:
+
+{3}دور از {4} از {5}. برای بازی در این مسابقه , به لینک زیر مراجعه کنید :
 
 {6}/tournaments.php?action=view&tid={2}
 
-You can disable new message notifications on your account options page:
+شما میتوانید دریافت این خبرنامه را متوقف کنید ، برای اینکار به لینک زیر مراجعه کنید :
 
 {6}/games.php?action=settings
 
-Thank you,
-{5} Staff
+با تشکر از شما,
+{5} گروه
 {6}";
 ?>
