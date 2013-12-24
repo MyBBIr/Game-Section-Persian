25c30b62397e00cd71f6095669bac9e123c8a2b8
ATofighi <alirtofighim@gmail.com>
2013-12-24 14:42:01 +0330

    Upload Game Plugin

---
+++ inc/languages/persian/games.lang.php
@@ -0,0 +1,181 @@
+<?php
+/***************************************************************************
+ *
+ *   Game Section for MyBB
+ *   Copyleft: © 2006-2010 The Game Section Development Group
+ *   
+ *   Website: http://www.gamesection.org
+ *   Translator: My-BB.Ir
+ *   
+ *   Last modified: 16/02/2010 by Paretje
+ *
+ ***************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************/
+
+$l['gamesection'] = "بخش بازی";
+$l['viewing_gamesection'] = "درحال مشاهده <a href=\"{1}\">بخش بازی</a>";
+$l['closed_title'] = "بسته شده";
+$l['closed'] = "بخش بازی بسته است.";
+$l['error'] = "خطا";
+$l['na'] = "نامشخص";
+$l['noinput'] = "شما چیزی را فراموش کرده‌اید.";
+
+$l['last_champions'] = "آخرین قهرمان‌ها";
+$l['your_favourites'] = "مورد علاقه‌های شما";
+$l['your_settings'] = "تنظیمات شما";
+$l['your_stats'] = "آمار شما";
+
+$l['whosonline'] = "افراد حاضر";
+$l['online_count'] = "<strong>حاضران:</strong> {1} | <strong>کاربران:</strong> {2} | <strong>مخفی:</strong> {3} | <strong>مهمان‌ها:</strong> {4} | <strong>ربات‌ها:</strong> {5} | <strong>رکورد:</strong> {6} در {8} در {7}";
+
+$l['stats'] = "آمار";
+$l['last_games'] = "آخرین بازی‌ها";
+$l['other_stats'] = "دیگر آمار";
+$l['mostplayed_games'] = "بیشترین بازی‌های انجام شده";
+
+$l['last_champs'] = "آخرین قهرمان‌ها";
+$l['last_champ'] = "آخرین قهرمان";
+$l['last_champs_sen'] = "<a href=\"games.php?action=stats&amp;uid={1}\">{2}</a> قهرمان جدید <a href=\"games.php?action=play&amp;gid={3}\">{4}</a> می‌باشد.";
+
+$l['last_score'] = "آخرین امتیاز اضافه شده";
+$l['last_scores'] = "آخرین امتیازات اضافه‌شده";
+$l['last_score_sen'] = "<a href=\"games.php?action=stats&amp;uid={1}\"><strong>{2}</strong></a> <strong>{3}</strong> امتیاز برای <a href=\"games.php?action=play&amp;gid={4}\"><strong>{5}</strong></a> به دست آورده است.";
+
+$l['bestplayers'] = "بهترین بازی‌کنندگان";
+$l['bestplayers_place_1'] = "رتبه اول";
+$l['bestplayers_place_2'] = "رتبه دوم";
+$l['bestplayers_place_3'] = "رتبه سوم";
+$l['bestplayers_sen'] = "<strong>{1}</strong><br />به همراه  <strong>{2}</strong> پیروزی.";
+
+$l['randomgames'] = "بازی‌های تصادفی";
+$l['randomgame'] = "بازی تصادفی";
+
+$l['short_second'] = "ثانیه";
+$l['short_seconds'] = "ثانیه";
+$l['short_minute'] = "دقیقه";
+$l['short_minutes'] = "دقیقه";
+$l['short_hour'] = "ساعت";
+$l['short_hours'] = "ساعت";
+$l['short_day'] = "روز";
+$l['short_days'] = "روز";
+
+$l['name'] = "نام";
+$l['description'] = "توضیحات";
+$l['menu'] = "منو";
+$l['champion'] = "قهرمان";
+
+$l['viewhighscores'] = "مشاهده امتیازات بالا";
+$l['yourhighscore'] = "بالاترین امتیاز شما:";
+$l['played'] = "چندبار بازی شده:";
+$l['lastplayed'] = "آخرین بازی انجام شده:";
+$l['lastplayed_sen'] = "{1} توسط <a href=\"games.php?action=stats&amp;uid={3}\">{2}</a>";
+$l['add_favourite'] = "افزودن به علاقه‌مندی‌ها";
+$l['delete_favourite'] = "حذف از علاقه‌مندی‌ها";
+$l['rating'] = "امتیاز دهی:";
+$l['rate'] = "رای";
+$l['champ'] = "<strong>{1}</strong><br />به همراه<br /><strong>{2}</strong><br />امتیاز قهرمان این بازی‌است.";
+
+$l['categories'] = "دسته بندی ها";
+$l['categorydoesntexist'] = "دسته بندی وجود ندارد..";
+$l['no_games'] = "هیچ بازی‌ای وجود ندارد.";
+
+$l['gamedoesntexist'] = "بازی وجود ندارد.";
+$l['back'] = "بازگشت به بخش بازی";
+$l['purpose'] = "هدف از بازی";
+$l['keys'] = "دکمه‌های بازی";
+$l['flashisntinstall'] = "متاسفیم، شما به <a href=\"http://www.macromedia.com/go/getflashplayer/\" target=\"_blank\">اجرا کننده فلاش</a> برای بازی د بخش بازی نیاز دارید.";
+
+$l['scoreadded'] = "امتیاز شما اضافه شد، <br /> شما به زودی می‌توانید نظر بگذارید.";
+$l['scoredontadded'] = "شما نتوانستید رکورد این بازی را بشکنید.<br />حال به صفحه امتیازات بازخواهید گشت.";
+$l['cheatscore'] = "به نظر می‌رسد شما میخواهید با تقلب امتیازی به دست اورید.!";
+$l['scoredoesntexist'] = "امتیازی وجود ندارد.";
+$l['commentsaved'] = "نظر شما اضافه شد.<br />حال باز خواهیدگشت..";
+
+$l['highscores'] = "بالاترین امتیازات برای {1}";
+$l['play_game'] = "بازی کردن {1}";
+$l['username'] = "نام‌کاربری";
+$l['score'] = "امتیاز";
+$l['comment'] = "نظر";
+$l['gainedat'] = "زمان به دست‌آوردن";
+$l['noscores'] = "هیچ امتیازی برای این بازی نیست..";
+
+$l['nochamps'] = "هیچ قهرمانی برای این انجمن نیست.";
+
+$l['statsofuser'] = "آمار برای {1}";
+$l['dataofuser'] = "تاریخ {1}";
+$l['rank'] = "رتبه";
+$l['timescore'] = "زمان بیشترین امتیاز";
+$l['first_places'] = "رتبه‌های اول:";
+$l['second_places'] = "رتبه‌های دوم:";
+$l['thirth_places'] = "رتبه‌های سوم:";
+$l['tenth_places'] = "۱۰ رتبه اول:";
+$l['total_scores'] = "مجموع امتیازات:";
+$l['bestplayerrank'] = "رتبه بهترین بازی کننده:";
+$l['userdoesntexist'] = "The member you specified is either invalid or doesn't exist.";
+
+$l['search'] = "Search";
+$l['all'] = "All";
+$l['noresults'] = "There are no results for your search.<br />You will be redirected to the Game Section.";
+$l['searching'] = "Thank you, your search has been submitted and you will now be taken to the results list.";
+$l['searchresults'] = "Search results";
+$l['searchresults_of'] = "Search results: {1}";
+$l['searchresults_name'] = "Name: {1}{2}";
+$l['searchresults_des'] = "Description: {1}{2}";
+$l['searchresults_cat'] = "Category: {1}";
+$l['gametitle'] = "Game title";
+$l['category'] = "Category";
+$l['bestscore'] = "Best score";
+$l['added'] = "Added on";
+
+$l['alreadyfavourite'] = "You already added this game to your favourites.";
+$l['added_favourite'] = "The game is added to your favourites.";
+$l['deleted_favourite'] = "The game is removed from your favourites.";
+
+$l['alreadyrated'] = "You added already a rating for this game.";
+$l['rate_game'] = "Rate {1}";
+$l['rated'] = "Your rating is added";
+
+$l['editsettings'] = "Edit Game Section Settings";
+$l['usedefauld'] = "Use default";
+$l['save'] = "Save";
+$l['settingssaved'] = "Your settings are saved.<br />You will be redirected.";
+
+$l['options_games'] = "Games Options";
+$l['options_scores'] = "Score Options";
+$l['options_themes'] = "Theme Options";
+
+$l['option_maxgames'] = "Maximum Games Per Page:";
+$l['option_maxgames_sen'] = "{1} games per page";
+
+$l['option_sortby'] = "Sort By:";
+$l['option_sortby_name'] = "Sort by name";
+$l['option_sortby_dateline'] = "Sort by date";
+$l['option_sortby_played'] = "Sort by played";
+$l['option_sortby_lastplayed'] = "Sort by last played";
+$l['option_sortby_rating'] = "Sort by rating";
+
+$l['option_order'] = "Order:";
+$l['option_order_asc'] = "Ascending";
+$l['option_order_desc'] = "Descending";
+
+$l['option_maxscores'] = "Maximum Scores Per Page:";
+$l['option_maxscores_sen'] = "{1} scores per page";
+
+$l['option_themes'] = "Game Section Theme:";
+?>
---
+++ inc/languages/persian/games_tasks.lang.php
@@ -0,0 +1,33 @@
+<?php
+/***************************************************************************
+ *
+ *   Game Section for MyBB
+ *   Copyleft: © 2006-2010 The Game Section Development Group
+ *   
+ *   Website: http://www.gamesection.org
+ *   Translate By: http://my-bb.ir
+ *   
+ *   Last modified: 03/01/2010 by Paretje
+ *
+ ***************************************************************************/
+
+/***************************************************************************
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************/
+
+$l['task_tournamentstatus_ran'] = "وظیفه وضعیت بخش بازی با موفقیت اجرا شد.";
+$l['task_gamescleanup_ran'] = "وظیفه پاک سازی بخش بازی با موفقیت انجام شد.";
+?>
