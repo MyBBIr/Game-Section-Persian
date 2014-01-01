47306a8702014c468018fd679724b938d7214faa
Mehdi Karimi <bomber.man87@yahoo.com>
2014-01-01 21:38:04 +0330

    تکمیل شد (mellat)
    ترجمه تکمیل شد

--- inc/languages/persian/admin/games_module_meta.lang.php
+++ inc/languages/persian/admin/games_module_meta.lang.php
@@ -5,6 +5,9 @@
  *   Copyleft: © 2006-2010 The Game Section Development Group
  *   
  *   Website: http://www.gamesection.org
+ * 
+ *   Persian translate by : My-bb.ir - mehdi karimi (Mellat)
+ *   Website: http://Mellat-design.ir
  *   
  *   Last modified: 07/02/2010 by Paretje
  *
@@ -27,74 +30,74 @@
  *
  ***************************************************************************/
 
-$l['gamesection'] = "Game Section";
+$l['gamesection'] = "بخش بازی";
 
-$l['nav_games'] = "Games";
-$l['nav_gamedata'] = "Gamedata";
-$l['nav_categories'] = "Categories";
-$l['nav_settings'] = "Settings";
-$l['nav_themes'] = "Themes";
-$l['nav_templates'] = "Templates";
-$l['nav_tools'] = "Tools";
-$l['nav_version'] = "Version Check";
+$l['nav_games'] = "بازی ها";
+$l['nav_gamedata'] = "دیتای بازی";
+$l['nav_categories'] = "دسته بندی ها";
+$l['nav_settings'] = "تنظیمات";
+$l['nav_themes'] = "قالب ها";
+$l['nav_templates'] = "پوسته ها";
+$l['nav_tools'] = "ابزار;
+$l['nav_version'] = "چک کردن نسخه";
 
-$l['can_manage_games'] = "Can Manage Games?";
-$l['can_manage_gamedata'] = "Can Manage Gamedata?";
-$l['can_manage_categories'] = "Can Manage Categories?";
-$l['can_manage_settings'] = "Can Edit Settings?";
-$l['can_manage_themes'] = "Can Manage Themes?";
-$l['can_manage_templates'] = "Can Manage Templates?";
-$l['can_manage_tools'] = "Can Run Tools?";
-$l['can_manage_version'] = "Can View Version Check?";
+$l['can_manage_games'] = "بازی ها را بتواند مدیریت کند؟";
+$l['can_manage_gamedata'] = "دیتای بازی را  بتواند مدیریت کند؟";
+$l['can_manage_categories'] = "دسته بندی ها را بتواند مدیریت کند؟";
+$l['can_manage_settings'] = "تنظیمات را بتواند ویرایش کند؟";
+$l['can_manage_themes'] = "قالب ها را بتواند مدیریت کند؟";
+$l['can_manage_templates'] = "پوسته ها را بتواند مدیریت کند؟";
+$l['can_manage_tools'] = "ابزار را بتواند اجرا کند؟";
+$l['can_manage_version'] = "بتواند نسخه ی بخش بازی را چک کند؟";
 
-$l['can_view_games'] = "Can view the Game Section?";
-$l['can_play_games'] = "Can play on the Game Section?";
+$l['can_view_games'] = "بخش بازی را بتواند مشاهده کند؟";
+$l['can_play_games'] = "بتواند در بخش بازی ، بازی کند؟";
 
-$l['tournaments'] = "Tournaments";
-$l['can_play_tournaments'] = "Can play tournaments?";
-$l['can_add_tournaments'] = "Can add tournaments?";
+$l['tournaments'] = "مسابقات";
+$l['can_play_tournaments'] = "بتواند مسابقه بازی کند؟";
+$l['can_add_tournaments'] = "بتواند مسابقه اضافه کند؟";
 
-$l['admin_log_games_games_add'] = "Added game #{1} ({2}) from category #{3} ({4})";
-$l['admin_log_games_games_add_simple'] = "Added game #{1} ({2}) from category #{3} ({4})";
-$l['admin_log_games_games_add_tar'] = "Added game #{1} ({2}) from category #{3} ({4})";
-$l['admin_log_games_games_edit'] = "Edited game #{1} ({2}) from category #{3} ({4})";
-$l['admin_log_games_games_delete'] = "Deleted game #{1} ({2}) from category #{3} ({4})";
+$l['admin_log_games_games_add'] = "بازی  #{1} ({2}) از دسته بندی #{3} ({4}) اضافه شد";
+$l['admin_log_games_games_add_simple'] = "بازی #{1} ({2}) از دسته بندی #{3} ({4}) افزوده شد";
+$l['admin_log_games_games_add_tar'] = "بازی #{1} ({2}) از دسته بندی #{3} ({4}) افزوده شد";
+$l['admin_log_games_games_edit'] = "بازی #{1} ({2}) از دسته بندی #{3} ({4}) ویرایش شد";
+$l['admin_log_games_games_delete'] = "بازی #{1} ({2}) از دسته بندی #{3} ({4}) حذف شد";
 
-$l['admin_log_games_gamedata_add'] = "Addeded gamedata-file {3} in {2}";
-$l['admin_log_games_gamedata_add_directory'] = "Added gamedata-directory {3} in {2}";
-$l['admin_log_games_gamedata_delete_file'] = "Deleted gamedata-file {3} in {2}";
-$l['admin_log_games_gamedata_delete_dir'] = "Deleted gamedata-directory {3} in {2}";
+$l['admin_log_games_gamedata_add'] = "دیتای بازی {3} در {2} افزوده شد";
+$l['admin_log_games_gamedata_add_directory'] = "دایرکتوری بازی {3} در {2} افزوده شد";
+$l['admin_log_games_gamedata_delete_file'] = "فایل دیتای بازی {3} در {2} حذف شد";
+$l['admin_log_games_gamedata_delete_dir'] = "دایرکتوری دیتای بازی {3} در {2} حذف شد";
 
-$l['admin_log_games_categories_add'] = "Added Game Section-category #{1} ({2})";
-$l['admin_log_games_categories_edit'] = "Edited Game Section-category #{1} ({2})";
-$l['admin_log_games_categories_delete'] = "Deleted Game Section-category #{1} ({2})";
+$l['admin_log_games_categories_add'] = "دسته بندی  #{1} ({2}) اضافه شد";
+$l['admin_log_games_categories_edit'] = "دسته بندی #{1} ({2}) ویرایش شد";
+$l['admin_log_games_categories_delete'] = "دسته بندی #{1} ({2}) ویرایش شد";
 
-$l['admin_log_games_settings_edit'] = "Changed the Game Section-settings.";
+$l['admin_log_games_settings_edit'] = "تنظیمات ویرایش شد";
 
-$l['admin_log_games_themes_add'] = "Added Game Section-theme #{1} ({2})";
-$l['admin_log_games_themes_import'] = "Imported Game Section-theme #{1} ({2})";
-$l['admin_log_games_themes_edit'] = "Edited Game Section-theme #{1} ({2})";
-$l['admin_log_games_themes_delete'] = "Deleted Game Section-theme #{1} ({2})";
-$l['admin_log_games_themes_export'] = "Exported Game Section-theme #{1} ({2})";
+$l['admin_log_games_themes_add'] = "قالب #{1} ({2}) افزوده شد";
+$l['admin_log_games_themes_import'] = "قالب #{1} ({2}) بارگذاری شد";
+$l['admin_log_games_themes_edit'] = "قالب #{1} ({2}) ویرایش شد";
+$l['admin_log_games_themes_delete'] = "قالب #{1} ({2}) حذف شد";
+$l['admin_log_games_themes_export'] = "از قالب #{1} ({2}) خروجی گرفته شد";
 
-$l['admin_log_games_templates_add'] = "Added Game Section-template #{1} ({2}) from teme #{3} ({4})";
-$l['admin_log_games_templates_edit'] = "Edited Game Section-template #{1} ({2}) from teme #{3} ({4})";
-$l['admin_log_games_templates_revert'] = "Reverted Game Section-template #{1} ({2}) from teme #{3} ({4})";
-$l['admin_log_games_templates_delete'] = "Deleted Game Section-template #{1} ({2}) from teme #{3} ({4})";
+$l['admin_log_games_templates_add'] = "پوسته  #{1} ({2}) از #{3} ({4}) افزوده شد";
+$l['admin_log_games_templates_edit'] = "پوسته #{1} ({2}) از #{3} ({4}) ویرایش شد";
+$l['admin_log_games_templates_revert'] = "قالب #{1} ({2}) از #{3} ({4}) برگشت داده شد";
+$l['admin_log_games_templates_delete'] = "پوسته #{1} ({2}) از #{3} ({4}) حذف شد";
 
-$l['admin_log_games_tools_repair_scores'] = "Repaired scores";
-$l['admin_log_games_tools_repair_champions'] = "Repaired champions";
-$l['admin_log_games_tools_repair_rating'] = "Repaired and recounted ratings";
-$l['admin_log_games_tools_repair_favourites'] = "Repaired favourites";
-$l['admin_log_games_tools_cleanup_gamedata'] = "Cleaned up gamedata";
-$l['admin_log_games_tools_repair_permissions'] = "Repaired permissions";
+$l['admin_log_games_tools_repair_scores'] = "امتیازات تعمیر شده";
+$l['admin_log_games_tools_repair_champions'] = "مسابقات تعمیر شده";
+$l['admin_log_games_tools_repair_rating'] = "تعمیر و بازشماری اعتبارها";
+$l['admin_log_games_tools_repair_favourites'] = "مورد علاقه های تعمیر شده";
+$l['admin_log_games_tools_cleanup_gamedata'] = "تمیز کردن دیتای بازی";
+$l['admin_log_games_tools_repair_permissions'] = "دسترسی های تعمیر شده";
 
-$l['noinput'] = "You forgot something!";
-$l['save'] = "Save";
+$l['noinput'] = "شما بعضی چیزها را فراموش کرده اید !";
+$l['save'] = "ذخیره";
 
-$l['multipage_pages'] = "<strong>Pages ({1})</strong>";
-$l['multipage_first'] = "First";
-$l['multipage_last'] = "Last";
-$l['multipage_previous'] = "Previous";
-$l['multipage_next'] = "Next";
+$l['multipage_pages'] = "<strong>صفحه های ({1})</strong>";
+$l['multipage_first'] = "اولین";
+$l['multipage_last'] = "آخرین";
+$l['multipage_previous'] = "قبلی";
+$l['multipage_next'] = "بعدی";
 ?>
